c0dbec0111e2ef7c6d89463dc4786d94cdc5b1f8
Neil Leonard <najleonard@gmail.com>
2017-01-02 09:23:46 -0800

    trying to use inline example

--- Tracker.Web/Controllers/TOAHController.cs
+++ Tracker.Web/Controllers/TOAHController.cs
@@ -7,14 +7,57 @@ using Tracker.Web.Models;
 
 namespace Tracker.Web.Controllers
 {
-    public class TOAHController : Controller
+    // public class TOAHController : Controller
+    // {
+    //     // GET: Inventory
+    //     public ActionResult Index()
+    //     {
+    //         LLInventoryRepository3 repo = new LLInventoryRepository3();
+    //         var model = repo.GetTOAHList();
+    //         return View(model);
+    //     }
+    // }
+    public class TOAHController : ApiController
     {
-        // GET: Inventory
-        public ActionResult Index()
+        [HttpGet, HttpPost, Route("api/upload-many")]
+        public IHttpActionResult Index()
         {
-            LLInventoryRepository3 repo = new LLInventoryRepository3();
-            var model = repo.GetTOAHList();
-            return View(model);
+            var request = HttpContext.Current.Request;
+            var settings = Properties.Settings.Default;
+
+            using (var db = new Database(settings.DbType, settings.DbConnection))
+            {
+                var response = new Editor(db, "users")
+                    .Model<UploadManyModel>()
+                    .Field(new Field("users.site")
+                        .Options(new Options()
+                            .Table("sites")
+                            .Value("id")
+                            .Label("name")
+                        )
+                    )
+                    .LeftJoin("sites", "sites.id", "=", "users.site")
+                    .MJoin(new MJoin("files")
+                        .Link("users.id", "users_files.user_id")
+                        .Link("files.id", "users_files.file_id")
+                        .Field(
+                            new Field("id")
+                                .Upload(new Upload(request.PhysicalApplicationPath + @"uploads\__ID____EXTN__")
+                                    .Db("files", "id", new Dictionary<string, object>
+                                    {
+                                        {"web_path", Upload.DbType.WebPath},
+                                        {"system_path", Upload.DbType.SystemPath},
+                                        {"filename", Upload.DbType.FileName},
+                                        {"filesize", Upload.DbType.FileSize}
+                                    })
+                                )
+                        )
+                    )
+                    .Process(request)
+                    .Data();
+
+                return Json(response);
+            }
         }
     }
 }
