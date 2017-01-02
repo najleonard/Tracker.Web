e0912c681b34115df368e4e1bcf3722a10f80ca1
Neil Leonard <najleonard@gmail.com>
2017-01-01 19:08:31 -0800

    test again

--- Tracker.Web/Controllers/HomeController.cs
+++ Tracker.Web/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Data.DataTable;
 
 namespace Tracker.Web.Controllers
 {
@@ -11,23 +10,7 @@ namespace Tracker.Web.Controllers
     {
         public ActionResult Index()
         {
-            ViewData["Message"] = "Welcome to ASP.NET MVC!";
-
-            DataTable dt = new DataTable("MyTable");
-            dt.Columns.Add(new DataColumn("Col1", typeof(string)));
-            dt.Columns.Add(new DataColumn("Col2", typeof(string)));
-            dt.Columns.Add(new DataColumn("Col3", typeof(string)));
-
-            for (int i = 0; i < 3; i++)
-            {
-                DataRow row = dt.NewRow();
-                row["Col1"] = "col 1, row " + i;
-                row["Col2"] = "col 2, row " + i;
-                row["Col3"] = "col 3, row " + i;
-                dt.Rows.Add(row);
-            }
-
-            return View(dt); //passing the DataTable as my Model
+            return View();
         }
 
         public ActionResult About()
