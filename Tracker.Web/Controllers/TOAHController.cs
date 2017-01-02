d7290292471b287df7a05e79d4d79505ee7bb7a1
Neil Leonard <najleonard@gmail.com>
2017-01-02 11:00:12 -0800

    back to ll Inentory

--- Tracker.Web/Controllers/TOAHController.cs
+++ Tracker.Web/Controllers/TOAHController.cs
@@ -1,10 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Data.Common;
-using System.Net.Http.Formatting;
+using System.Linq;
 using System.Web;
-using System.Web.Http;
-using DataTables;
+using System.Web.Mvc;
 using Tracker.Web.Models;
 
 namespace Tracker.Web.Controllers
@@ -14,38 +12,9 @@ namespace Tracker.Web.Controllers
         // GET: Inventory
         public ActionResult Index()
         {
-
             LLInventoryRepository3 repo = new LLInventoryRepository3();
             var model = repo.GetTOAHList();
             return View(model);
+        }
     }
-    
-//    [AcceptVerbs(HttpVerbs.Get|HttpVerbs.Post)]
-    
-//     public class TOAHController : Controller
-//     {
-//         public ActionResult Table()
-//         {
-//             var settings = Properties.Settings.Default;
-//             var formData = HttpContext.Request.Form;
-    
-//             using (var db = new Database(settings.DbType, settings.DbConnection))
-//             {
-//                 var response = new Editor(db, "ItemOrder")
-//                     .Model<ItemOrder>()
-//                     .Field(new Field("start_date")
-//                         .Validator(Validation.DateFormat(
-//                             Format.DATE_ISO_8601,
-//                             new ValidationOpts { Message = "Please enter a date in the format yyyy-mm-dd" }
-//                         ))
-//                         .GetFormatter(Format.DateSqlToFormat(Format.DATE_ISO_8601))
-//                         .SetFormatter(Format.DateFormatToSql(Format.DATE_ISO_8601))
-//                     )
-//                     .Process(formData)
-//                     .Data();
-    
-//                 return Json(response, JsonRequestBehavior.AllowGet);
-//             }
-//         }
-//     }
 }
