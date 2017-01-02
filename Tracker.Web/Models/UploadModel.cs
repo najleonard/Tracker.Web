0d8cadad772a33e9398cb90fae85936eaa2babf2
Neil Leonard <najleonard@gmail.com>
2017-01-02 09:51:19 -0800

    added uploadmodel

---
+++ Tracker.Web/Models/UploadModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using DataTables;
+
+namespace Tracker.Web.Models
+{
+    public class UploadModel
+    {
+        public string first_name { get; set; }
+
+        public string last_name { get; set; }
+
+        public string phone { get; set; }
+
+        public string city { get; set; }
+
+        public int image { get; set; }
+    }
+}
