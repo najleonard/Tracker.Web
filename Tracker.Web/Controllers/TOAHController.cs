3f6d1773bb90fb1da963306576b24b87fc587c89
Neil Leonard <najleonard@gmail.com>
2017-01-02 09:53:23 -0800

    amended TOAH contorller again

--- Tracker.Web/Controllers/TOAHController.cs
+++ Tracker.Web/Controllers/TOAHController.cs
@@ -20,18 +20,29 @@ namespace Tracker.Web.Controllers
     //     }
     // }
     public class TOAHController : ApiController
-    {
+   {
         [HttpGet, HttpPost, Route("api/upload")]
-        public IHttpActionResult Index()
+        public IHttpActionResult Staff()
         {
             var request = HttpContext.Current.Request;
             var settings = Properties.Settings.Default;
 
             using (var db = new Database(settings.DbType, settings.DbConnection))
             {
-                var response = new Editor(db, "Inventory")
-                    .Model<Inventory>()
+                var response = new Editor(db, "users")
+                    .Model<UploadModel>()
                     .Field(new Field("image")
+                        .SetFormatter(Format.IfEmpty(null))
+                        .Upload(new Upload(request.PhysicalApplicationPath + @"uploads\__ID____EXTN__")
+                            .Db("files", "id", new Dictionary<string, object>
+                            {
+                                {"web_path", Upload.DbType.WebPath},
+                                {"system_path", Upload.DbType.SystemPath},
+                                {"filename", Upload.DbType.FileName},
+                                {"filesize", Upload.DbType.FileSize}
+                            })
+                        )
+                    )
                     .Process(request)
                     .Data();
 
