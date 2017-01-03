a7d2625298d46456051d85bbec980d812fda3e3f
Neil Leonard <najleonard@gmail.com>
2017-01-02 16:56:38 -0800

    asdfasdf

--- Tracker.Web/App_Start/BundleConfig.cs
+++ Tracker.Web/App_Start/BundleConfig.cs
@@ -21,11 +21,15 @@ namespace Tracker.Web
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/dataTables.editor.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/editor.dataTables.min.css"
+                      ));
+
         }
     }
 }
