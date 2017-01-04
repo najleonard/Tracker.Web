bdf3d8714a69a5efc8bacae83faf8ca7c6b24f4f
Neil Leonard <najleonard@gmail.com>
2017-01-04 12:20:50 -0800

    does it need namespace

--- Tracker.Web/Models/OrderRepository.cs
+++ Tracker.Web/Models/OrderRepository.cs
@@ -8,14 +8,16 @@ namespace Tracker.Web.Models
 {
     public class OrderRepository
     {
-        public int UpdateOrderShipped(int OrderId)
+        public int UpdateOrderShipped(ShippedModel myShippedOrder)
         {
             int affectedRows = 0;
             using (var db = new trackerwebdbEntities())
             {
                 //update tagid, friendly name, description and (future) image blob
                 var model = db.Orders.Where(x => x.Id == OrderId).First();
-                model.Shipped = 1;
+                model.Shipped = myShippedOrder.OrderId;
+                model.ShippedDate = myShippedOrder.ShippedDate;
+                
                 affectedRows = db.SaveChanges();
             }
             return affectedRows;
