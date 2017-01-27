8ff6dd2d610e221eff656d727e91cb6fb0167b33
Neil Leonard <najleonard@gmail.com>
2017-01-26 21:05:27 -0800

    dingdong

--- Tracker.Web/Controllers/TableController.cs
+++ Tracker.Web/Controllers/TableController.cs
@@ -81,7 +81,7 @@ namespace Tracker.Web.Controllers
     
             using (var db = new Database(settings.DbType, settings.DbConnection))
             {
-                DtResponse response  = new Editor(db, "Order","Id")
+                DtResponse response  = new Editor(db, "Order")
                     .Model<JoinOrderClient>()
                     .Field(new Field("Order.InventoryItem1").SetFormatter( Format.IfEmpty( null ) ))
                     .Field(new Field("Order.InventoryItem2").SetFormatter( Format.IfEmpty( null ) ))
