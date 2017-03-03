d50ba2a83ed6101608c9717323aa5bfead589774
Neil Leonard <najleonard@gmail.com>
2017-03-03 11:01:42 -0800

    added api for llinventory

--- Tracker.Web/Controllers/LLInventoryController.cs
+++ Tracker.Web/Controllers/LLInventoryController.cs
@@ -17,4 +17,34 @@ namespace Tracker.Web.Controllers
             return View(model);
         }
     }
+
+    [RoutePrefix("api/inventory")]
+    public class LLInventoryController : ApiController
+    {   
+        [HttpGet]
+        [HttpPost]
+        [Route("getdata")]
+        public IHttpActionResult Table()
+        {
+            var settings = Properties.Settings.Default;
+            var request = HttpContext.Current.Request;
+    
+            using (var db = new Database(settings.DbType, settings.DbConnection))
+            {
+                DtResponse response  = new Editor(db, "Inventory","Id")
+                    .Model<JoinOrderClient>()
+                    .Field(new Field("Inventory.Id").SetFormatter( Format.IfEmpty( null ) ))
+                    .Field(new Field("Inventory.Product_sku").SetFormatter( Format.IfEmpty( null ) ))
+                    .Field(new Field("Inventory.Size").SetFormatter( Format.IfEmpty( null ) ))
+                    .Field(new Field("Inventory.Color").SetFormatter( Format.IfEmpty( null ) ))
+                    .Field(new Field("Inventory.Extras").SetFormatter( Format.IfEmpty( null ) ))
+                    .Field(new Field("Inventory.Location").SetFormatter( Format.IfEmpty( null ) ))
+                    .Field(new Field("Inventory.rfid_tag").SetFormatter( Format.IfEmpty( null ) ))
+                    .Process(request)
+                    .Data();
+                    
+                return Json(response);
+            }
+        }
+    }    
 }
