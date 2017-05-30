19bf54c8921a7238688edb98e719403c354182cd
Neil Leonard <najleonard@gmail.com>
2017-05-30 00:44:45 -0700

    update location to order

--- Tracker.Web/Models/OrderRepository.cs
+++ Tracker.Web/Models/OrderRepository.cs
@@ -29,33 +29,36 @@ namespace Tracker.Web.Models
                 model.ShippedDate = Convert.ToDateTime(myShippedOrder.ShippedDate);
                 db.SaveChanges();
 
-               /* var model2 = db.Inventories.Where(x => x.Id == model.InventoryItem1).First();
+                var model2 = db.Inventories.Where(x => x.Id == model.InventoryItem1).First();
                 model2.Location = myShippedOrder.OrderId.ToString();
-                //db.SaveChanges();
+                db.SaveChanges();
                 
                 var model3 = db.Inventories.Where(x => x.Id == model.InventoryItem2).First();
                 model3.Location = myShippedOrder.OrderId.ToString();
-                //db.SaveChanges();
+                db.SaveChanges();
                 
                 var model4 = db.Inventories.Where(x => x.Id == model.InventoryItem3).First();
                 model4.Location = myShippedOrder.OrderId.ToString();
-                //db.SaveChanges();
+                db.SaveChanges();
                 
                 var model5 = db.Inventories.Where(x => x.Id == model.InventoryItem4).First();
                 model5.Location = myShippedOrder.OrderId.ToString();
+                db.SaveChanges();
 
                 var model6 = db.Inventories.Where(x => x.Id == model.InventoryItem5).First();
                 model6.Location = myShippedOrder.OrderId.ToString();
+                db.SaveChanges();
 
                 var model7 = db.Inventories.Where(x => x.Id == model.InventoryItem6).First();
                 model7.Location = myShippedOrder.OrderId.ToString();
+                db.SaveChanges();
 
                 var model8 = db.Inventories.Where(x => x.Id == model.InventoryItem7).First();
                 model8.Location = myShippedOrder.OrderId.ToString();
+                db.SaveChanges();
 
                 var model9 = db.Inventories.Where(x => x.Id == model.InventoryItem8).First();
                 model9.Location = myShippedOrder.OrderId.ToString();
-                */
                 db.SaveChanges();
                 
             }
