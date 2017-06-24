95b20980ea60dbc761bca3cb15a21a80b94fa7e8
danielle <danielle@laceandliberty.com>
2017-06-24 08:01:32 +0000

    redreict

--- Tracker.Web/Login.aspx.cs
+++ Tracker.Web/Login.aspx.cs
@@ -38,7 +38,7 @@ namespace WebFormsIdentity
                 var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
 
                 authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, userIdentity);
-                Response.Redirect("~/Login.aspx");
+                Response.Redirect("~/");
             }
             else
             {
