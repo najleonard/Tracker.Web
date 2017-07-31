a1a4bf0119240d09a9106d83e91fe12821b96d58
Neil Leonard <najleonard@gmail.com>
2017-07-30 18:09:32 -0700

    added sms2

--- Tracker.Web/Controllers/SmsController.cs
+++ Tracker.Web/Controllers/SmsController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Twilio.AspNet.Mvc;
+using Twilio.TwiML;
 
 namespace Tracker.Web.Controllers
 {
-    public class SmsController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View ();
-        }
-    }
+	public class SmsController : TwilioController
+	{
+		[HttpPost]
+		public ActionResult Index()
+		{
+			var messagingResponse = new MessagingResponse();
+			messagingResponse.Message("The Robots are coming! Head for the hills!");
+
+			return TwiML(messagingResponse);
+		}
+	}
 }
