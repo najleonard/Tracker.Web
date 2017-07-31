b893dff32856817b4d2bf426753821bf56c6c9fc
Neil Leonard <najleonard@gmail.com>
2017-07-30 18:09:11 -0700

    added sms

---
+++ Tracker.Web/Controllers/SmsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tracker.Web.Controllers
+{
+    public class SmsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View ();
+        }
+    }
+}
