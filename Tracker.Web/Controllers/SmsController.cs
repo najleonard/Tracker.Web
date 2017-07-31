ebe2e3baa5dad447aa2f2d33fab97638f9f57e27
Neil Leonard <najleonard@gmail.com>
2017-07-30 18:51:56 -0700

    forward sms

--- Tracker.Web/Controllers/SmsController.cs
+++ Tracker.Web/Controllers/SmsController.cs
@@ -22,6 +22,10 @@ namespace Tracker.Web.Controllers
 			string To = Request["To"];
 			string Body = Request["Body"];
 
+            //forward to L&L staff numbers
+			var accountSid = "AC01316b284a8c2fe1a2024489794cada6";
+			var authToken = "08a1f0d1a1f00414358c6e42dd16e807";
+			TwilioClient.Init(accountSid, authToken);
 
 			var to = new PhoneNumber("+14156963814");
 			var from = new PhoneNumber("+14152002558");
@@ -31,7 +35,9 @@ namespace Tracker.Web.Controllers
 				from: from,
 				body: Body
 			);
-			return TwiML(message);
+
+			return Content(message.Sid);
+
 		}
 
         public ActionResult SendSMS()
