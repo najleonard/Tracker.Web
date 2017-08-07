a5d01af8371eb41935623400163e4de930f4992d
Neil Leonard <najleonard@gmail.com>
2017-08-07 15:00:12 -0700

    updated

---
+++ Tracker.Web/Controllers/OrderInventoryController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+//using System.Net.Http.Formatting;
+using System.Web;
+//using System.Web.Http;
+//using System.Web.Http.Description;
+using DataTables;
+//using System.Net.Http;
+using Tracker.Web.Models;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace Tracker.Web.Controllers
+{  
+    public class OrderInventoryController : Controller
+    {
+        // GET: Monitor
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
